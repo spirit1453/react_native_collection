bbee9539b011603bb4c0d334a7e5364a5b2d0cf4
spirit <catchspirit37@gmail.com>
2018-09-04 16:50:38 +0800

    z

---
+++ src/widget/DelayIndicator.js
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react'
+import {
+  View,
+  ActivityIndicator
+} from 'react-native'
+import PropTypes from 'prop-types'
+
+export default class My extends Component<{}> {
+  constructor (props) {
+    super(props)
+    this.state = {
+      content: null
+    }
+  }
+
+  componentDidMount () {
+    this.hasMounted = true
+
+    setTimeout(() => {
+      if (this.hasMounted) {
+        this.setState({
+          content: (
+            <ActivityIndicator size='large'/>
+          )})
+      }
+    }, this.props.delay)
+  }
+
+  componentWillUnmount () {
+    this.hasMounted = false
+  }
+
+  render () {
+    return (
+      <View style={{display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        {this.state.content}
+      </View>
+    )
+  }
+}
+
+My.defaultProps = {
+  delay: 1000
+}
+
+My.propTypes = {
+  delay: PropTypes.number
+}
