4d8eded46fb8ca0763eb56033c5b4bdec3fb7a35
spirit <catchspirit37@gmail.com>
2018-09-20 14:38:05 +0800

    z - Thu Sep 20 2018 14:38:05 GMT+0800 (China Standard Time)

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,11 +1,7 @@
-const childProcess = require('child_process')
+
 const {ModuleUtil} = require('@ys/collection')
-const {isDependency} = ModuleUtil
+const {installGit} = ModuleUtil
 const path = require('path')
 const rootPath = path.resolve(__dirname, '../')
 
-if (!isDependency(rootPath)) {
-  childProcess.execSync(`
-     npm i @ys/eslint-config-rn -D
-    `)
-}
+installGit(rootPath)
