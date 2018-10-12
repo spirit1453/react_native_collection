6c6187d13c94700a2f877258a65339877e1e4126
spirit <catchspirit37@gmail.com>
2018-10-12 16:38:20 +0800

    z - Fri Oct 12 2018 16:38:20 GMT+0800 (CST)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 import ScanView from './view/ScanView'
 import DelayIndicator from './widget/DelayIndicator'
 import HeaderRightButton from './widget/HeaderRightButton'
+import TextInputWrapper from './widget/TextInputWrapper'
 const commonUtil = require('./util/commonUtil')
 
 const common = {
   commonUtil,
   ScanView,
   DelayIndicator,
-  HeaderRightButton
+  HeaderRightButton,
+  TextInputWrapper
 }
 
 Object.freeze(common)
